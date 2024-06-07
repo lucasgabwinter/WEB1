5ae83b18d5e481702b106e34b2016cf2eef686b0
lucasgabwinter <lucasgabwinter@gmail.com>
2024-06-07 16:06:03 -0300

    calc

--- main.js
+++ main.js
@@ -21,11 +21,61 @@
 
 // alert(`The temperature is ${fahrenheit.toFixed(2)} degrees fahrenheit`);
 
-let colorList = [];
+// let fruitsList = ['banana', 'orange', 'apple', 'papaya', 'lemon'];
 
-for (let i = 0; i < 3; i++) {
-  let tempColor = prompt('Type a colour');
-  colorList.push(tempColor);
-}
+// let numberList = document.getElementById('numberList');
 
-document.getElementById('colorList').innerHTML = colorList;
+// for (let i = 0; i < fruitsList.length; i++) {
+//   let item = document.createElement('li');
+//   item.textContent = fruitsList[i];
+//   numberList.appendChild(item);
+// }
+
+// colors = ['orange', 'red', 'blue', 'green', 'purple'];
+
+// function changeColor() {
+//   let body = document.getElementById('body');
+//   let number = Math.floor(Math.random() * colors.length);
+//   let color = colors[number];
+//   body.style.backgroundColor = color;
+// }
+
+// function checkAge() {
+//   let age = document.getElementById('age').value;
+//   age > 18 ? alert('You are an adult') : alert('You are not an adult');
+// }
+
+// let checkButton = document.getElementById('checkButton');
+// checkButton.addEventListener('click', () => {
+//   checkAge();
+// });
+
+let userNum1 = document.getElementById('userNum1');
+let userNum2 = document.getElementById('userNum2');
+let calculate = document.getElementById('calculate');
+let operator = document.getElementById('operator');
+let result = document.getElementById('result');
+
+calculate.addEventListener('click', () => {
+  console.log(operator);
+  let num1 = Number(userNum1.value);
+  let num2 = Number(userNum2.value);
+  console.log(num1);
+  console.log(typeof num1);
+  switch (operator.value) {
+    case '+':
+      result.innerHTML = num1 + num2;
+      break;
+    case '-':
+      result.innerHTML = num1 - num2;
+      break;
+    case '*':
+      result.innerHTML = num1 * num2;
+      break;
+    case '/':
+      result.innerHTML = num1 / num2;
+      break;
+    default:
+      break;
+  }
+});
